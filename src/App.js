996177ebbf47ec0f1614c8435b848d253ee2d1ee
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-04 08:59:06 +1200

    Init for Ajax

--- src/App.js
+++ src/App.js
@@ -3,19 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+
+	componentDidMount(){
+		fetch(`https://api.example.com/items`)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					this.setState({
+						isLoaded: true,
+						items: result
+					});
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	}
+
+	render(){
+		const { error, isLoaded, items } = this.state;
+		if(error){
+			return <div>Error: {error.message}</div>;
+		} else if(!isLoaded){
+			return <div>Loading...</div>;
+		} else {
+			return(
+				<ul>
+					{items.map(item => (
+						<li key={item.name}>
+							{item.name} {item.price}
+						</li>
+					))}
+				</ul>
+			);
+		}
+	}
 }
 
 export default App;
