63d70299bee47335282bc36e26c12614100ecd64
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-04 09:17:33 +1200

    Basic server setup and routing

--- node/index.js
+++ node/index.js
@@ -7,3 +7,9 @@ app.use(function(req,res,next){
 	console.log(`${req.method} request for ${req.url}`);
 	next();
 });
+
+app.get(`/getJSON`, (req,res) => { res.json(data) });
+
+app.set(`port`, (process.env.PORT || 4000));
+
+app.listen(app.get(`port`), () => { console.log(`Server is running on port ${app.get(`port`)}`) });
